47e99ac2d6a8f7fadf8b9375015196e39e39656e
ronaldalbertjr <ronaldalbert1609@gmail.com>
2017-08-11 10:34:41 -0300

    EditorWindow

---
+++ Assets/Editor/LerpTestEditor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(LerpTest))]
+public class LerpTestEditor :  Editor
+{
+    public override void OnInspectorGUI()
+    {
+        LerpTest test = target as LerpTest;
+
+        test.min = EditorGUILayout.FloatField("Minimun Value:", test.min);
+        if (test.min > test.max) test.min = test.max;
+        test.max = EditorGUILayout.FloatField("Maximun Value:", test.max);
+        test.t = EditorGUILayout.Slider("T value:", test.t, 0f, 1f);
+        EditorGUI.BeginDisabledGroup(true);
+        EditorGUILayout.FloatField("Resultado:", Mathf.Lerp(test.min, test.max, test.t));
+        EditorGUI.EndDisabledGroup();
+    }
+}
---
+++ Assets/LerpTest.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class LerpTest : MonoBehaviour
+{
+    public float min;
+    public float max;
+    public float t;
+}
